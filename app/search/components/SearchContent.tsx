6fb2a4e4054fead8852d7584598cd10e6e9c9695
Michael Mooney <mikeymooney1991@gmail.com>
2023-06-28 18:28:29 -0500

    Favorites functionality!

--- app/search/components/SearchContent.tsx
+++ app/search/components/SearchContent.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import MediaItem from "@/components/MediaItem";
 import { Song } from "@/types";
+import MediaItem from "@/components/MediaItem";
+import LikeButton from "@/components/LikeButton";
 
 interface SearchContentProps {
   songs: Song[];
@@ -12,11 +13,11 @@ const SearchContent: React.FC<SearchContentProps> = ({ songs }) => {
     return (
       <div
         className="
-          flex
-          flex-col
-          gap-y-2
-          w-full
-          px-6
+          flex 
+          flex-col 
+          gap-y-2 
+          w-full 
+          px-6 
           text-neutral-400
         "
       >
@@ -24,13 +25,15 @@ const SearchContent: React.FC<SearchContentProps> = ({ songs }) => {
       </div>
     );
   }
+
   return (
-    <div className="flex felx-col gap-y-2 w-full px-6">
-      {songs.map((song) => (
+    <div className="flex flex-col gap-y-2 w-full px-6">
+      {songs.map((song: Song) => (
         <div key={song.id} className="flex items-center gap-x-4 w-full">
           <div className="flex-1">
             <MediaItem onClick={() => {}} data={song} />
           </div>
+          <LikeButton songId={song.id} />
         </div>
       ))}
     </div>
